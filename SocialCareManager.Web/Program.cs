7f35df8a5fd284e416fdc0b345db5353d075e78a
Ezat Akbari <ezatakbari23@hotmail.com>
2026-06-21 20:33:20 +0200

    Add ServiceUsers page to Blazor frontend

--- SocialCareManager.Web/Program.cs
+++ SocialCareManager.Web/Program.cs
@@ -1,6 +1,7 @@
 using SocialCareManager.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
